27cb73b79d4c60aa6823aa46c17e4fa7ff32385e
Bohdan <koqfloqf@gmail.com>
2025-08-30 13:10:25 +0300

    some chngs

---
+++ dicts.py
@@ -0,0 +1,6 @@
+d = {'one': 1, 'two': 2, 'three': 3, 'four': 4}
+d2 = {'four': 10, 'five': 5}
+d3 = {**d, **d2}
+
+print(d3)
+
---
+++ walrus.py
@@ -0,0 +1,7 @@
+# if d:= int(input()):
+#     print(d)
+s = 0
+while (d := int(input())) > 1:
+    s += d
+
+print(s)
