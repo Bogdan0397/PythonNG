6da5be81cdaf656ce2fd0c712f0b6c277bdac4a3
Bohdan <koqfloqf@gmail.com>
2025-09-02 17:51:19 +0300

    some chngs

--- cycles.py
+++ cycles.py
@@ -17,24 +17,28 @@
 #     return [1 if inp.count(x) == 1 else inp.count(x) for x in set(inp)]
 #
 # print(set_reducer([0, 4, 6, 8, 8, 8, 5, 5, 7]))
-def set_reducer(input):
+# def set_reducer(input):
+#
+#     counter = 1
+#     a = []
+#     for i in range(len(input)-1):
+#         if input[i]==input[i+1]:
+#             counter +=1
+#         else:
+#             a.append(counter)
+#             counter = 1
+#
+#     a.append(counter)
+#
+#
+#
+#     if len(a) == 1:
+#          return a[0]
+#     else:
+#         return set_reducer(a)
+#
+# print(set_reducer([0, 4, 6, 8, 8, 8, 5, 5, 7]))
 
-    counter = 1
-    a = []
-    for i in range(len(input)-1):
-        if input[i]==input[i+1]:
-            counter +=1
-        else:
-            a.append(counter)
-            counter = 1
+l = [1,2,3,4,5,6,7,8,9]
 
-    a.append(counter)
-
-
-
-    if len(a) == 1:
-         return a[0]
-    else:
-        return set_reducer(a)
-
-print(set_reducer([0, 4, 6, 8, 8, 8, 5, 5, 7]))
+print(sorted(l))
--- str.py
+++ str.py
@@ -1,5 +1,18 @@
-s ='hello'
-a = ['22','gga','22','55125521']
+# s ='hello'
+# a = ['22','gga','22','55125521']
+#
+# print(', '.join(x for x in a if len(x)>=3))
 
-print(', '.join(x for x in a if len(x)>=3))
+s = ')('
 
+
+def valid_parentheses(paren_str):
+    if len(paren_str) == 0:
+        return True
+    if '()' not in paren_str:
+        return False
+
+    s = paren_str.replace('()', '')
+    return valid_parentheses(s)
+
+print(valid_parentheses(s))
