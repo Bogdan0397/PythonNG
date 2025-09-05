4291acca9857c86b449339f7f7bf3707f87758ca
Bohdan <koqfloqf@gmail.com>
2025-09-05 14:12:37 +0300

    some chngs

--- strs.py
+++ strs.py
@@ -1,9 +1,18 @@
-s = input().lower()
-res = ''
-for i in range(len(s)-1):
-    if s[i] == 'р' and s[i+1] == 'а':
-        res += str(i) +" "
-print(res.strip()) if len(res) > 0 else print('-1')
-
-
-
+# s = input().lower()
+# res = ''
+# for i in range(len(s)-1):
+#     if s[i] == 'р' and s[i+1] == 'а':
+#         res += str(i) +" "
+# print(res.strip()) if len(res) > 0 else print('-1')
+#
+#
+#
+s = 'Welcome'
+it = reversed(s)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
---
+++ zeros.py
@@ -0,0 +1,10 @@
+def move_zeros(lst):
+
+    for i in lst:
+        if i == 0:
+            lst.remove(i)
+            lst.append(i)
+    return lst
+
+
+print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
