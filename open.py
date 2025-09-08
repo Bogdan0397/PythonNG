59993fd54a7cd1160e9c490212fb9e8eadd73ba2
Bohdan <koqfloqf@gmail.com>
2025-09-08 15:17:21 +0300

    some chngs

--- open.py
+++ open.py
@@ -1,7 +1,15 @@
+#
+# try:
+#     file = open("myfile2.txt", encoding="utf-8")
+#     s = file.readlines()
+#     file.close()
+# except FileNotFoundError:
+#     print("File not found")
 
-try:
-    file = open("myfile2.txt", encoding="utf-8")
-    s = file.readlines()
-    file.close()
-except FileNotFoundError:
-    print("File not found")
+with open("myfile.txt", 'a+') as file:
+    file.seek(0)
+    print(file.readlines())
+
+
+
+
