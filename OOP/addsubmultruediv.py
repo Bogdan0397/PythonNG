dbbd4ff6facb0693a1cbe33e990d7d3b47cd07dc
Bohdan <koqfloqf@gmail.com>
2025-10-04 00:03:45 +0300

    some chngs

---
+++ OOP/addsubmultruediv.py
@@ -0,0 +1,49 @@
+class Clock:
+    __DAY = 86400
+
+    def __init__(self,seconds: int):
+        if not isinstance(seconds,int):
+            raise ValueError('Not int')
+        self.seconds = seconds% self.__DAY
+
+    def get_time(self):
+        s = self.seconds % 60
+        m = (self.seconds//60) % 60
+        h = (self.seconds//3600) % 24
+
+        return f"{self.__get_formated(h)}:{self.__get_formated(m)}:{self.__get_formated(s)}"
+
+    @classmethod
+    def __get_formated(cls,x):
+        return str(x).rjust(2,"0")
+
+    def __add__(self, other):
+        if not isinstance(other, (int, Clock)):
+            raise ArithmeticError('other nor int not Clock')
+
+        sc = other
+        if isinstance(sc,Clock):
+            sc = other.seconds
+
+        return Clock(self.seconds + sc)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __iadd__(self, other):
+        if not isinstance(other, (int,Clock)):
+            raise ArithmeticError('Worng Value')
+
+        sc = other
+        if isinstance(other,Clock):
+            sc = other.seconds
+
+        self.seconds+= sc
+        return self
+
+
+
+c1 = Clock(1000)
+c1 += 400
+print(c1.get_time())
+
